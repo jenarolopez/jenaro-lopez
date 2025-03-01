64b06612aa2685440970bcc01251f48627911cc0
jenarolopez <jenjenlopez37@gmail.com>
2025-03-01 20:03:34 +0800

    Update Archive page layout and add Highlights component
    - Wrap Archive page content with Body and add Highlights component
    - Import Body and Highlights components
    - Add background color to the main container

--- src/app/component/WorkExperience.tsx
+++ src/app/component/WorkExperience.tsx
@@ -27,7 +27,7 @@ export default function Workexperience() {
       <div className="mt-0 mb-20">
         <a
           className="inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-teal-300 focus-visible:text-teal-300 font-semibold text-slate-200 group/link text-base"
-          href={'/resume.pdfz'}
+          href={'/resume.pdf'}
           target="_blank"
           rel="noreferrer noopener"
           aria-label="View Full Résumé (opens in a new tab)"
